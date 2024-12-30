67aa94b75dbf7ec7916e63a0455b8a1e5626b921
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-12-30 11:57:35 +0530

    Initialised SmartContract

---
+++ src/Contract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+contract Contract {
+    struct Credit {
+    string typeofcredit;
+    uint256 quantity;
+    string certificate_uri;
+    uint256 expiry_date;
+}
+
+}
